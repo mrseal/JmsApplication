2a06c0a1f7ec84f8c27e3f5853c982504dc20e1d
eccnffi <fei.chen@ericsson.com>
2015-09-16 07:47:48 +0100

    Remove header

--- sample-ejb-service/sample-ejb-service-war/src/main/java/com/cf/test/jms/NewEventSenderServlet.java
+++ sample-ejb-service/sample-ejb-service-war/src/main/java/com/cf/test/jms/NewEventSenderServlet.java
@@ -6,18 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/*------------------------------------------------------------------------------
- *******************************************************************************
- * COPYRIGHT Ericsson 2012
- *
- * The copyright to the computer program(s) herein is the property of
- * Ericsson Inc. The programs may be used and/or copied only with written
- * permission from Ericsson Inc. or in accordance with the terms and
- * conditions stipulated in the agreement/contract under which the
- * program(s) have been supplied.
- *******************************************************************************
- *----------------------------------------------------------------------------*/
-
 @WebServlet("/jmstestnew")
 public class NewEventSenderServlet extends HttpServlet {
 
