1167e32ef787498d84108b5a68d4e5e7249afe6c
eccnffi <fei.chen@ericsson.com>
2015-09-16 07:52:20 +0100

    commnent out old code

--- sample-ejb-service/sample-ejb-service-war/src/main/java/com/cf/test/jms/EventSender.java
+++ sample-ejb-service/sample-ejb-service-war/src/main/java/com/cf/test/jms/EventSender.java
@@ -1,35 +1,35 @@
-package com.cf.test.jms;
-
-import javax.annotation.Resource;
-import javax.ejb.*;
-import javax.jms.*;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@Stateless
-public class EventSender {
-
-    private final static String MESSAGE = "Hello, TEST Hello, TEST Hello, TEST Hello, TEST";
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    @Resource(name = "java:/JmsXA")
-    private ConnectionFactory connectionFactory;
-
-    @Resource(name = "java:/queue/testqueue")
-    private Queue queue;
-
-    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
-    public void send() {
-        try {
-            final Connection connection = connectionFactory.createConnection();
-            final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            final Message message = session.createTextMessage(MESSAGE);
-            final MessageProducer producer = session.createProducer(queue);
-            producer.send(message);
-        } catch (final Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
+//package com.cf.test.jms;
+//
+//import javax.annotation.Resource;
+//import javax.ejb.*;
+//import javax.jms.*;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//@Stateless
+//public class EventSender {
+//
+//    private final static String MESSAGE = "Hello, TEST Hello, TEST Hello, TEST Hello, TEST";
+//
+//    private final Logger logger = LoggerFactory.getLogger(getClass());
+//
+//    @Resource(name = "java:/JmsXA")
+//    private ConnectionFactory connectionFactory;
+//
+//    @Resource(name = "java:/queue/testqueue")
+//    private Queue queue;
+//
+//    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+//    public void send() {
+//        try {
+//            final Connection connection = connectionFactory.createConnection();
+//            final Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+//            final Message message = session.createTextMessage(MESSAGE);
+//            final MessageProducer producer = session.createProducer(queue);
+//            producer.send(message);
+//        } catch (final Exception e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
--- sample-ejb-service/sample-ejb-service-war/src/main/java/com/cf/test/jms/EventSenderService.java
+++ sample-ejb-service/sample-ejb-service-war/src/main/java/com/cf/test/jms/EventSenderService.java
@@ -1,30 +1,30 @@
-package com.cf.test.jms;
-
-import javax.ejb.*;
-import javax.inject.Inject;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-@Stateless
-public class EventSenderService {
-
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
-    @Inject
-    EventSender sender;
-
-    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
-    public void send(final int num) {
-        logger.info("Sending {} messages", num);
-        for (int i = 0; i < num; i++) {
-            try {
-                logger.info("Sending message {}", i);
-                sender.send();
-            } catch (final Exception e) {
-                logger.info("==== SEND METHOD GOT EXCEPTION: ", e);
-            }
-        }
-    }
-
-}
+//package com.cf.test.jms;
+//
+//import javax.ejb.*;
+//import javax.inject.Inject;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//
+//@Stateless
+//public class EventSenderService {
+//
+//    private final Logger logger = LoggerFactory.getLogger(getClass());
+//
+//    @Inject
+//    EventSender sender;
+//
+//    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+//    public void send(final int num) {
+//        logger.info("Sending {} messages", num);
+//        for (int i = 0; i < num; i++) {
+//            try {
+//                logger.info("Sending message {}", i);
+//                sender.send();
+//            } catch (final Exception e) {
+//                logger.info("==== SEND METHOD GOT EXCEPTION: ", e);
+//            }
+//        }
+//    }
+//
+//}
